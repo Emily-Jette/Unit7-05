6648998c4c026084abb4c0ec5895bc597462c7d7
Emily Jette <emily.jette@ocsbstudent.ca>
2021-03-25 13:45:17 +0000

    fixed spacing

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// This code checks what the user inputs as their age and the day to see what it is time for them to do. 
-let day = null 
+// This code checks what the user inputs as their age and the day to see what it is time for them to do.
+let day = null
 let age = 0
 
 document.getElementById('button').addEventListener('click', timeTo)
@@ -10,7 +10,7 @@ function timeTo () {
   day = document.getElementById('day').value
   if (age < 18 && day !== 'sunday' && day !== 'saturday') {
     document.getElementById('answer').innerHTML = 'Time to go to school!'
-  } else if (age >= 18 && day !== 'sunday' && day !== 'saturday'){
+  } else if (age >= 18 && day !== 'sunday' && day !== 'saturday') {
     document.getElementById('answer').innerHTML = 'Time to go to work!'
   } else {
     document.getElementById('answer').innerHTML = 'Time to relax for the weekend!'
